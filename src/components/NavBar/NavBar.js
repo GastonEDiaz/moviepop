7fe06d52dd37d79dc2ae7b3a25dc60102cfc9ef9
Gaston <gastonestebandiaz@gmail.com>
2020-08-03 00:15:30 -0300

    Add element select with navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,23 +2,33 @@ import React, { useState } from "react";
 import { Home, Search } from "react-feather";
 import "./NavBar.css";
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const NavBar = ( props ) => {
   let history = useHistory(props.history);
   const setRoute = route => history.push(route);
   // Set hook to change status buttons
   const [buttonActive, setButtonActive] = useState(history.location.pathname);
+  const dispatch = useDispatch();
+
+  const setOption = (e,route) => {
+    e.preventDefault(); 
+    setButtonActive(route); 
+    setRoute(route); 
+    dispatch({type: 'CLEAR_STORE'});
+  }
+
   return (
     <header className={"NavBar"}>
       <button 
         className={buttonActive === "/catalog" ? "active" : ""}
-        onClick={(e) => { e.preventDefault(); setButtonActive("/catalog"); setRoute('/catalog') }}
+        onClick={(e) => { setOption(e, "/catalog") }}
       >
         <Search />
       </button>
       <button
         className={buttonActive === "/" ? "active" : ""}
-        onClick={(e) => { e.preventDefault(); setButtonActive("/"); setRoute('/') }}
+        onClick={(e) => { setOption(e,'/') }}
       >
         <Home />
       </button>
