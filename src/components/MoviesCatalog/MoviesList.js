4ea832ff01d8face305b028a4edfa10ae62b9122
Gaston <gastonestebandiaz@gmail.com>
2020-08-05 07:02:52 -0300

    Add loader spinner

--- src/components/MoviesCatalog/MoviesList.js
+++ src/components/MoviesCatalog/MoviesList.js
@@ -2,25 +2,40 @@ import React,{useState,useEffect} from 'react';
 import "./MovieList.css"
 import ElementCatalog from '../ElementCatalog/ElementCatalog';
 import { getData } from '../../services';
+import Loader from 'react-loader-spinner';
 
 const MoviesList = () => {
     const [movies, setMovies] = useState([]);
+    const [isLoadignElements, setLoading] = useState(false);
     useEffect(() => {
+        setLoading(true);
         async function getMovies() {
             const movies = await getData()
             setMovies(movies);
         }
         getMovies();
+        setLoading(false);
      }, [])
     return(
         <div className="container" >
             <div className="row no-gutters row-cols-3 row-cols-sm-3 row-cols-md-3 " >
                 {
-                    movies && movies.map( (movie, index) => {
+                    !isLoadignElements && movies && movies.map( (movie, index) => {
                         return <ElementCatalog movie={movie} key={index} />
                     })
                 }
             </div>
+            {
+                isLoadignElements && 
+                <div className="LoaderContainer" >
+                    <Loader
+                        type="Puff"
+                        color="#FFFFFF"
+                        height={100}
+                        width={100}
+                    />
+                </div>
+                }
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 ReactDOM.render(
   <React.StrictMode>
