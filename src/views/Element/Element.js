d2f12a510c0121465a1a65d7c2eb5e0aa4348588
Gaston <gastonestebandiaz@gmail.com>
2020-08-05 07:23:49 -0300

    Fix texts

--- src/views/Element/Element.js
+++ src/views/Element/Element.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { useSelector } from "react-redux";
-import movieCover from "../../statics/movie-cover.jpg";
+import movieCover from "../../statics/movie-cover.png";
 import "./Element.css"
 import StarsCounter from '../../components/StarsCounter/StarsCounter';
 
-const Element = (element) => {
+const Element = () => {
     const movie = useSelector(state => state.elementReducer.movie )
     return (
         <div className="Element" >
             <div className="CoverContainer" >
-                <img alt="coverimage" src={movie.show && movie.show.image && movie.show.image.original ? movie.show.image.original : movieCover} className="pure-img" />
+                <img alt="coverimage" src={movie.show && movie.show.image && movie.show.image.original ? movie.show.image.original : movieCover} className="img-fluid" />
                 <StarsCounter rating={movie.show.rating.average} />
             </div>
-            <h2>{movie.show.name}</h2>
+            <label>{movie.show.name}</label>
             <div className="InfoContainer" >
                 <div className="Info" >
-                    <label>Lenguaje:</label>
+                    <label className="labelInfo" >Lenguaje:</label>
                     <p>{movie.show && movie.show.language}</p>
                 </div>
                 <div className="Info" >
-                    <label>Género:</label>
+                    <label className="labelInfo" >Género:</label>
                     <p>{movie.show && movie.show.genres.toString()}</p>
                 </div>
                 <div className="Info" >
-                    <label>Fecha de estreno:</label>
+                    <label className="labelInfo" >Fecha de estreno:</label>
                     <p>{movie.show && movie.show.premiered}</p>
                 </div>
             </div>
-            <h3>Sinopsis</h3>
+            <label>Sinopsis</label>
             <div dangerouslySetInnerHTML={{__html: movie.show && movie.show.summary}} />
         </div>
     );
